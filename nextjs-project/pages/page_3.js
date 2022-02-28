223d7d86c94e4b02190c95dad4d129e27476dbfc
ysho <ysho2@illinois.edu>
2022-02-28 17:22:04 -0600

    Restructured code

--- nextjs-project/pages/page_3.js
+++ nextjs-project/pages/page_3.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Fragment } from 'react';
+import { wordCloudProcessor } from '../resource/tools'
 import * as d3 from "d3";
 import * as cloud from 'd3-cloud';
 import React, {useState, useRef, useEffect} from 'react';
@@ -26,9 +27,10 @@ function PageThree({ final_data }) {
     // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
     var layout = cloud()
     .size([width, height])
-    .words(data.map(function(d) { return {text: d}; }))
+    .words(data.map(function(d) { return {text: d, size:d.size}; }))
     .padding(5)
-    .fontSize(20)
+    .fontSize(function(d) { return d.size; })
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
     .on("end", draw);
     layout.start();
 
@@ -65,29 +67,6 @@ function PageThree({ final_data }) {
     </Fragment>
   );
   
-}
-
-
-// Processes texts and performs data preparation to be passed into
-// Partially taken from https://observablehq.com/@d3/word-cloud
-function wordCloudProcessor(json_result) {
-  
-  // var JSON_data = JSON.parse(json_result);
-  let items = json_result["items"];
-  var full_text = "";
-  for (let index = 0; index < items.length; index++) {
-    full_text = full_text + items[index]["snippet"]["topLevelComment"]["snippet"]["textOriginal"] + " ";
-  }
-  var stopwords = new Set("i,me,my,myself,we,us,our,ours,ourselves,you,your,yours,yourself,yourselves,he,him,his,himself,she,her,hers,herself,it,its,itself,they,them,their,theirs,themselves,what,which,who,whom,whose,this,that,these,those,am,is,are,was,were,be,been,being,have,has,had,having,do,does,did,doing,will,would,should,can,could,ought,i'm,you're,he's,she's,it's,we're,they're,i've,you've,we've,they've,i'd,you'd,he'd,she'd,we'd,they'd,i'll,you'll,he'll,she'll,we'll,they'll,isn't,aren't,wasn't,weren't,hasn't,haven't,hadn't,doesn't,don't,didn't,won't,wouldn't,shan't,shouldn't,can't,cannot,couldn't,mustn't,let's,that's,who's,what's,here's,there's,when's,where's,why's,how's,a,an,the,and,but,if,or,because,as,until,while,of,at,by,for,with,about,against,between,into,through,during,before,after,above,below,to,from,up,upon,down,in,out,on,off,over,under,again,further,then,once,here,there,when,where,why,how,all,any,both,each,few,more,most,other,some,such,no,nor,not,only,own,same,so,than,too,very,say,says,said,shall".split(","));
-  var words = full_text.split(/[\s.]+/g)
-  .map(w => w.replace(/^[“‘"\-—()\[\]{}]+/g, ""))
-  .map(w => w.replace(/[;:.!?()\[\]{},"'’”\-—]+$/g, ""))
-  .map(w => w.replace(/['’]s$/g, ""))
-  .map(w => w.substring(0, 30))
-  .map(w => w.toLowerCase())
-  .filter(w => w && !stopwords.has(w))
-
-  return words;
 }
 
 export async function getServerSideProps() {
