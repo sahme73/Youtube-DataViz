e301fa6eb38647eb10c4600c4ae837f87ff99e3c
S4MU3LZ <42338321+S4MU3LZ@users.noreply.github.com>
2022-03-03 13:01:20 +0800

    update fetcher

--- nextjs-project/pages/api/youtube.js
+++ nextjs-project/pages/api/youtube.js
@@ -1,6 +1,14 @@
-export default async (req, res) => {
-    var key = process.env.YOUTUBE_KEY
-    const response = await fetch('https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key='+key)
-    const json = await response.json()
-    res.status(200).json(json)
+export default async (req, res) => { // simple fetcher that fetches all popular videos from the YouTube API's mostPopular chart
+    var key = process.env.YOUTUBE_KEY // use your own API key
+    let response = await fetch('https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key='+key)
+    let json = await response.json()
+    let items = json.items // extract information
+    let nextPageToken = json.nextPageToken
+    while (nextPageToken) {
+        response = await fetch('https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&pageToken='+nextPageToken+'&regionCode=US&key='+key)
+        json = await response.json()
+        items = items.concat(json.items)
+        nextPageToken = json.nextPageToken
+    }
+    return res.status(200).json(items) // return a raw json file
 }
