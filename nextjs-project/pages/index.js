dee62dbf1506e46a8a6dca465d02131094130f83
Safeer Ahmed <sahme73@illinois.edu>
2022-03-03 22:47:57 -0600

    Added css style modules to the homepage with animated buttons. Updated Next.js version.

--- nextjs-project/pages/index.js
+++ nextjs-project/pages/index.js
@@ -1,33 +1,47 @@
+import styles from '../styles/HomePage.module.css';
 import Link from 'next/link';
 import { Fragment } from 'react';
 import * as d3 from "d3";
 
 function HomePage() {
+
+  // removes any svg graphs from homepage -- prevents infinite graph generation
   try {
     d3.selectAll("svg").remove();
   } catch (error) {
-    console.log(error);
+    //console.log(error);
   }
 
   return (
     <Fragment>
-      <h1>The Home Page - YouTube Analysis</h1>
-      <ul>
+      <h1 className={styles.title}>YouTube Analysis</h1>
+      <ul className={styles.list}>
         <li>
-          <Link href='/page_1'>
-            Page 1 - D3 Bar Graph Test
-          </Link>
+          <button className={styles.button}>
+            <span>
+              <Link href='/page_1'>
+                D3 Bar Graph Example
+              </Link>
+            </span>  
+          </button>
         </li>
         <li>
-          <Link href='/page_2'>
-            Page 2 - Views vs Likes (Aug 2020)
-          </Link>
+          <button className={styles.button}>
+            <span>
+              <Link href='/page_2'>
+                Views vs Likes (August 2020)
+              </Link>
+            </span>  
+          </button>
         </li>
-
         <li>
-          <Link href='/page_3'>
-            Page 3 - Word Cloud
-          </Link>
+          <button className={styles.button}>
+            <span>
+              <Link href='/page_3'>
+                Word Cloud
+              </Link>
+            </span>  
+          </button>
         </li>
       </ul>
     </Fragment>
