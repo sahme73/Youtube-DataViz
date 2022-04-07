6bac6361a39b8e468fb7411684f693a290af4416
ysho <ysho2@illinois.edu>
2022-04-07 14:30:29 -0500

    Fixed issue with Bubble Chart not showing

--- nextjs-project/resource/tools.js
+++ nextjs-project/resource/tools.js
@@ -1,17 +1,18 @@
 // Top 100 most popular videos are extracted from the YouTube API
 // Processed to make an array of JS Objects for D3.js input
 function processVideos(videos) {
+  let temp = videos["items"];
   let final_videos = [];
 
-  if (videos == null) {
+  if (temp == null) {
     return final_videos;
   }
-  for (let index = 0; index < videos.length; index++) {
+  for (let index = 0; index < temp.length; index++) {
     let curr_obj = {};
-    curr_obj["id"] = videos[index]["snippet"]["categoryId"];
-    curr_obj["title"] = videos[index]["snippet"]["title"];
-    curr_obj["viewCount"] = videos[index]["statistics"]["viewCount"];
-    curr_obj["likeCount"] = videos[index]["statistics"]["likeCount"];
+    curr_obj["id"] = temp[index]["snippet"]["categoryId"];
+    curr_obj["title"] = temp[index]["snippet"]["title"];
+    curr_obj["viewCount"] = temp[index]["statistics"]["viewCount"];
+    curr_obj["likeCount"] = temp[index]["statistics"]["likeCount"];
     final_videos.push(curr_obj);
   }
   return final_videos;
