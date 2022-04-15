4e83b38c8d3e54883b52f75adc841af13fc9ad7a
S4MU3LZ <42338321+S4MU3LZ@users.noreply.github.com>
2022-04-16 03:39:47 +0800

    changed fetcher to use useSWR

--- nextjs-project/pages/getMostPopularVideos.js
+++ nextjs-project/pages/getMostPopularVideos.js
@@ -1,42 +1,32 @@
-import React, {useEffect, useState} from "react";
-// require('dotenv').config();
-// import useSWR from "swr";
+import useSWR from "swr";
 
-//'AIzaSyC9woSZMjGt2yS_o_TlpQyIhOuNwwRxJyU' // use your own API key
-// console.log(key)
+//get api key
+const key = process.env.YOUTUBE_KEY
 
+//endpoint for api call
+const videosEndpoint = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key="
 
-// const getData = (...args) => fetch(...args).then((response) => response.json());
+const getVideos = async () => {
+    const response = await fetch (videosEndpoint+key);
+    return await response.json();
+}
 
-function VideosApp(){
-    // const [key, setKey] = useState(null)
-    // console.log(key)
-    // return (<div>{key}</div>)
-    const key = process.env.YOUTUBE_KEY//'AIzaSyC9woSZMjGt2yS_o_TlpQyIhOuNwwRxJyU'// process.env.YOUTUBE_TOKEN does not work
-    const videosEndpoint = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key="
-    // console.log(videosEndpoint)
-    const [data, setData] = useState(null)
-    const [isLoading, setLoading] = useState(false)
+// function that returns top 50 most popular videos
+function getMostPopularVideos(){
 
-    useEffect(() => {
-        setLoading(true)
-        // setKey(process.env.YOUTUBE_TOKEN)
-        fetch(videosEndpoint + key)
-            .then((res) => res.json())
-            .then((data) => {
-                setData(data)
-                setLoading(false)
-            })
-    }, [])
+    // useSWR hook to fetch data
+    const {data: videos, error } = useSWR(videosEndpoint+key, getVideos)
 
-    if (isLoading) return <p>Loading...</p>
-    if (!data) return <p>No data</p>
-    // console.log(data)
+    if (!videos) return <p>Loading...</p>
+    if (error) return <p>Error loading data</p>
+
+    // return data
     return (
         <div>
-            {JSON.stringify(data.items)}
+            {JSON.stringify(videos.items)}
         </div>
     );
+    // return data.items;
 };
 
-export default VideosApp;
+export default getMostPopularVideos;
