ae92a60ae36c4bf4ebc5cdfe6aefb57ceb691854
ysho <ysho2@illinois.edu>
2022-04-26 12:08:44 -0500

    Removed Unnecessary Comments

--- nextjs-project/pages/page_4.js
+++ nextjs-project/pages/page_4.js
@@ -117,30 +117,6 @@ function PageFour({ final_data, categories_json }) {
             .style("border-radius", "5px")
             .style("padding", "10px")
             .style("color", "white")
-        // leaf.selectAll('circle')
-        // .on("mouseover", function(event,d) {
-        //   // console.log(d)
-        //   tooltip
-        //   .transition()
-        //   .duration(200)
-        //   tooltip
-        //   .style("opacity", 1)
-        //   .html(D[d.data].title)
-        //   .style("left", (d.x+30) + "px")
-        //   .style("top", (d.y+30) + "px")
-        // })
-        // .on("mousemove", function(event,d) {
-        //   tooltip
-        //   .style("left", (d.x+30) + "px")
-        //   .style("top", (d.y+30) + "px")
-        // })
-        // .on("mouseleave", function(event,d) {
-        //   tooltip
-        //   .transition()
-        //   .duration(200)
-        //   .style("opacity", 0)
-        // })
-
       }
     
       return Object.assign(svg.node(), {scales: {color}});
